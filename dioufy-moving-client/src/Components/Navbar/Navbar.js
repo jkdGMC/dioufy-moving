0e711b7020fae933f506a59b0f54b96fe9111950
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-10-03 17:56:54 +0000

    Adding components & assets,then styling first view section

---
+++ dioufy-moving-client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import logo from '../../Assets/main-logo/black.svg'
+import './Navbar.css'
+
+const Navbar = () => {
+
+    const [navbar, setNavbar] = useState(false);
+
+    const background = () => (window.scrollY >=50) ? setNavbar(true) : setNavbar(false)
+    window.addEventListener('scroll', background)
+
+  return (
+    <div className='Navbar'>
+         <nav className={navbar ? 'navbar active' : 'navbar'}>
+            <div className="logo-container">
+                <img className="dioufy-moving-logo" src={logo} alt="Dioufy moving" />
+            </div>
+            <div className="navigation-panel">
+                <ul>
+                    <li>Accueil</li>
+                    <li>A propos</li>
+                    <li>Offres</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="btn-panel">
+                <div className="login-container-btn">
+                    <i className="fa-solid fa-user"></i>
+                    <input className='login-btn' type="button" value='Se connecter'/>
+                </div>
+                <div className="signup-container-btn">
+                    <input className='signup-btn' type="button" value="S'inscrire"/>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
