79c65e7b4cb9be55b0acf0c812b13c537f36ea93
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-10-04 11:29:04 +0000

    Adding header description text

--- dioufy-moving-client/src/Components/Header/Header.js
+++ dioufy-moving-client/src/Components/Header/Header.js
@@ -5,10 +5,18 @@ import Navbar from '../Navbar/Navbar'
 const Header = () => {
   return (
     <div className='header'>
-      <Navbar />
-      <div className="header-gif">
-      TESH
+    <Navbar />
+      <div className="header-desc">
+        <div className="header-desc-container">
+          <div className="header-image">
+            <h1>DIOUFY MOVING</h1>
+          </div>
+          <div className="header-txt">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, at! Rem eius ullam alias, veniam inventore obcaecati iusto sint? Et tenetur optio nam debitis, quasi rem labore obcaecati exercitationem ratione.</p>
+          </div>
+        </div>
       </div>
+
     </div>
   )
 }
