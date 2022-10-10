df97799839fc7ae79d0988eef0c754b30a8059b4
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-10-10 12:08:17 +0000

    ...

--- dioufy-moving-client/src/App.js
+++ dioufy-moving-client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import './App.css';
 import Apropos from './Components/Apropos/Apropos';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+
+import './App.css';
+
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 
 function App() {
   return (
     <div className="App">
+    <LazyLoadComponent>
       <Header />
+    </LazyLoadComponent>
       <Apropos />
       <Footer />
     </div>
